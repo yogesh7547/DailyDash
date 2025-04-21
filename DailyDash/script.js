448352d1b564bdfe62839d64bd53f2b59e424479
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-04-21 16:53:22 +0530

    basic structure of ui , weather api  and date
    basic layout done . 
    fetching and using weather api done.
     adding date done.

---
+++ DailyDash/script.js
@@ -0,0 +1,50 @@
+
+/*|| WEATHER CARD */
+
+let city;
+let country;
+let temp;
+let humidity;
+let conditionText;
+let conditionIcon;
+let feelsLikeTemp;
+
+
+
+
+
+fetch("https://api.weatherapi.com/v1/current.json?key=794ecc358ea146be9a391627252104&q=mumbai&aqi=no")
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    city=data.location.name;
+    country=data.location.country;
+    temp=data.current.temp_c;
+    humidity= data.current.humidity;
+    conditionIcon=data.current.condition.icon;
+    conditionText=data.current.condition.text;
+    feelsLikeTemp=data.feelslike_c;
+    
+
+    document.querySelector(".temp").innerHTML=`${temp}°C`;
+    document.querySelector(".condition img").src="https:" +conditionIcon;
+    document.querySelector(".condition h4").innerHTML=conditionText;
+    document.querySelector(".location").innerHTML=`${city} , ${country}`;
+    document.querySelector(".humidity").innerHTML=`Humidity : ${humidity}`;
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+/*|| DATE-TIME CARD */
+
+const today= new Date();
+const day = String(today.getDate()).padStart(2, '0');
+const month= String(today.getMonth()+1).padStart(2,'0');
+const year=String(today.getFullYear());
+const time=String(today.getTime())
+
+let fullDate= `${day}/${month}<br><span>${year}</span>`
+
+document.querySelector(".date-time").innerHTML=fullDate;
